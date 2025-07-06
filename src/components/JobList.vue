29e8922c6940455988d21691f8cd3da467f3839d
mostafa <mustafaabdelra7man@gmail.com>
2025-07-06 06:49:51 +0300

    finish jobs pages with nessary server connections and edit vite to be dynamic with the deployment

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,10 +1,10 @@
 <script setup>
-import {ref,defineProps} from 'vue'
-import jobData from '@/jobs.json'
+import {defineProps,onMounted, reactive} from 'vue'
 import JobCardList from './JobCardList.vue'
 import { RouterLink } from 'vue-router'
+import axios from 'axios'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 
-const jobs=ref(jobData.jobs)
 const props=defineProps({
     limit:Number,
     showMore:{
@@ -13,14 +13,33 @@ const props=defineProps({
     }
 })
 
+const state = reactive({
+    jobs:[],
+    isLoading:true
+})
+onMounted(async()=>{
+    try{
+        const response=await axios.get("/api/jobs")
+         state.jobs=response.data
+         console.log(state.jobs)
+    }catch(error){
+        console.log("error fetching jobs data",error)
+    }finally{
+        state.isLoading=false
+    }
+})
 </script>
 
 <template>
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6  text-center">All Jobs</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobCardList v-for="job in jobs.slice(0,limit||jobs.length)" :key="job.id" :job="job"/>
+            <!--show loading using spinner while loading is true-->
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader color="gray" />
+            </div>
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobCardList v-for="job in state.jobs.slice(0,limit||state.jobs.length)" :key="job.id" :job="job"/>
             </div>
         </div>
     </section>
